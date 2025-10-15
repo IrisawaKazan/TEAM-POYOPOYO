72210a32d338b20528e386c09e7e1786cb0857d6
fs820 <241308@st.yoshida-g.ac.jp>
2025-10-15 10:58:50 +0900

    Player歩きTest

--- player.cpp
+++ player.cpp
@@ -5,6 +5,9 @@
 //
 //************************************************************
 #include "player.h"
+#include "math_T.h"
+
+// 静的メンバ変数の定義
 
 // コンストラクタ
 CPlayer::CPlayer()
@@ -22,6 +25,10 @@ CPlayer::~CPlayer()
 HRESULT CPlayer::Init(void)
 {
 	CModelCharacter::Init("data\\Model\\MOTION\\player.txt",5.0f);
+
+	// 移動処理 (プロト用) sato Add
+	CModelCharacter::SetRot(D3DXVECTOR3(0.0f, TEST_MOVE_ANGLE, 0.0f));
+	CModelCharacter::SetRotDest(D3DXVECTOR3(0.0f, TEST_MOVE_ANGLE, 0.0f));
 	return S_OK;
 }
 
@@ -34,6 +41,7 @@ void CPlayer::Uninit(void)
 // 更新
 void CPlayer::Update(void)
 {
+	TestMove(); // プロト用移動処理 sato Add
 	CModelCharacter::Update();
 }
 
@@ -61,4 +69,22 @@ CPlayer* CPlayer::Create(D3DXVECTOR3 pos, D3DXVECTOR3 rot)
 		return nullptr;
 	}
 
+}
+
+//*******************************
+// プロト用移動処理 sato Add
+//*******************************
+void CPlayer::TestMove(void)
+{
+	// 移動処理 (プロト用) sato Add
+	D3DXVECTOR3 rot = CModelCharacter::GetRot();
+	D3DXVECTOR3 pos = CModelCharacter::GetPos();
+	CModelCharacter::SetMove(D3DXVECTOR3(-sinf(rot.y) * TEST_MOVE_SPEED, 0.0f, -cosf(rot.y) * TEST_MOVE_SPEED + (0.0f - pos.z) * 0.01f));
+	D3DXVECTOR3 posOld = CModelCharacter::GetPosOld();
+	if (std::abs(pos.x) > TEST_MOVE_LIMIT && std::abs(posOld.x) <= TEST_MOVE_LIMIT)
+	{// 移動制限を超えたら
+		rot.y += D3DX_PI; // 角度を反転
+		Normalize(rot.y, &rot.y);
+		CModelCharacter::SetRotDest(rot);
+	}
 }
--- playermanager.cpp
+++ playermanager.cpp
@@ -6,6 +6,9 @@
 //************************************************************
 #include "playermanager.h"
 
+// 静的メンバ変数の定義
+const D3DXVECTOR3 CPlayerManager::SPAWN_POS = D3DXVECTOR3(-1800.0f, 0.0f, 0.0f); // NPCの出現位置 sato Add
+
 // コンストラクタ
 CPlayerManager::CPlayerManager()
 {
@@ -24,9 +27,9 @@ HRESULT CPlayerManager::Init(void)
 	// ローカル変数
 	CPlayer* Info = {};
 
-	for (int nCnt = 0; nCnt < 5; nCnt++)
+	for (int nCnt = 0; nCnt < NUM_PLAYER; nCnt++)
 	{
-		Info = CPlayer::Create(D3DXVECTOR3(50.0f * nCnt,0.0f,0.0f), VEC3_NULL);
+		Info = CPlayer::Create(SPAWN_POS + D3DXVECTOR3(50.0f * nCnt, 0.0f, 0.0f), VEC3_NULL);
 
 		// 情報を前から追加していく
 		m_pPlayer.push_back(Info);
