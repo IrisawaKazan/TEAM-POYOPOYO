03a93ae98b61554d265030a2d1ca8fbbf77b01fd
KensakuHatori <241316@st.yoshida-g.ac.jp>
2025-10-16 14:28:55 +0900

    リフト

--- uplift.cpp
+++ uplift.cpp
@@ -1,35 +1,68 @@
+//****************************************************************
+//
+// リフトの処理[uplift.cpp]
+// Author Kensaku Hatori
+//
+//****************************************************************
+
+// インクルード
 #include "manager.h"
 #include "uplift.h"
 
+//***************************************
+// コンストラクタ
+//***************************************
 CUpLift::CUpLift()
 {
 }
 
+//***************************************
+// デストラクタ
+//***************************************
 CUpLift::~CUpLift()
 {
 }
 
+//***************************************
+// 初期化処理
+//***************************************
 HRESULT CUpLift::Init(void)
 {
 	CObjectX::Init();
 
-	m_CollisionShape = std::make_unique<btBoxShape>(btVector3(10.0f,10.0f,10.0f));
+	// 当たり判定を生成
+	m_CollisionShape = std::make_unique<btBoxShape>(btVector3(5.0f,5.0f,5.0f));
 
-	btScalar mass = 10.0f; // 質量を1以上にすることで動的剛体になる
+	// 質量を設定
+	btScalar mass = 2.5f;
+
+	// 移動方向を設定
 	btVector3 inertia(0, 0, 0);
+
+	// 計算
 	m_CollisionShape->calculateLocalInertia(mass, inertia);
 
+	// トランスフォームを宣言、初期化
 	btTransform transform;
 	transform.setIdentity();
 	transform.setOrigin(btVector3(GetPosition().x, GetPosition().y + 20.0f, GetPosition().z));
 
+	// モーションステートを宣言
 	btDefaultMotionState* motionState = new btDefaultMotionState(transform);
+
+	// リジットボディーの情報を宣言
 	btRigidBody::btRigidBodyConstructionInfo info(mass, motionState, m_CollisionShape.get());
 
+	// リジットボディを生成
 	m_RigitBody = std::make_unique<btRigidBody>(info);
+
+	// 移動制限
 	m_RigitBody->setLinearFactor(btVector3(1, 1, 1));
 
+	// ユーザーポインタを設定
 	m_RigitBody->setUserPointer(this);
+
+	// 静的が動的か設定
 	m_RigitBody->setActivationState(DISABLE_DEACTIVATION);
 
 	CManager::GetDynamicsWorld()->addRigidBody(m_RigitBody.get());
@@ -37,6 +70,9 @@ HRESULT CUpLift::Init(void)
 	return S_OK;
 }
 
+//***************************************
+// 終了処理
+//***************************************
 void CUpLift::Uninit(void)
 {
 	// 剛体の削除
@@ -56,35 +92,89 @@ void CUpLift::Uninit(void)
 	CObjectX::Uninit();
 }
 
+//***************************************
+// 更新処理
+//***************************************
 void CUpLift::Update(void)
 {
+	// 制限距離
+	D3DXVECTOR3 Dest = m_ApperPos + (m_Velocity * m_Limit);
+	float LmitDis = D3DXVec3Length(&Dest);
+
+	// 位置
 	btVector3 Pos;
 	btTransform trans;
 	trans.setIdentity();
 	m_RigitBody->getMotionState()->getWorldTransform(trans);
 	Pos = trans.getOrigin();
+
+	// 今の位置から初期位置のベクトルの長さを計算
+	Dest = D3DXVECTOR3(Pos.x(), Pos.y(), Pos.z()) - m_ApperPos;
+	float NowDis = D3DXVec3Length(&Dest);
+
+	// スイッチが押されていなくて制限まで移動したら
+	if (!m_Switch && NowDis > LmitDis)
+	{
+		// 移動量を反転
+		m_Velocity = -m_Velocity;
+
+		// フラグを有効化
+		m_Switch = true;
+	}
+	// スイッチが押されていて初期位置に近かったら
+	else if (m_Switch && NowDis < 10.0f)
+	{
+		// 移動量を反転
+		m_Velocity = -m_Velocity;
+
+		// フラグを無効か
+		m_Switch = false;
+	}
+
+	// 移動量を設定
 	m_RigitBody->setLinearVelocity(btVector3(m_Velocity.x, m_Velocity.y, m_Velocity.z));
+
+	// 見た目の位置を設定
 	SetPosition({ Pos.x() ,Pos.y() ,Pos.z() });
+
+	// 更新
 	CObjectX::Update();
 }
 
+//***************************************
+// 描画
+//***************************************
 void CUpLift::Draw(void)
 {
 	CObjectX::Draw();
 }
 
+//***************************************
+// 生成
+//***************************************
 CUpLift* CUpLift::Create(D3DXVECTOR3 Pos, D3DXVECTOR3 Velocity, float Limit)
 {
+	// インスタンスを宣言
 	CUpLift* pInstance = nullptr;
+
+	// メモリ確保
 	pInstance = new CUpLift;
 
+	// 確保できたら
 	if (pInstance != nullptr)
 	{
+		// 情報を設定
 		pInstance->SetPosition(Pos);
 		pInstance->m_Velocity = Velocity;
 		pInstance->m_Limit = Limit;
+		pInstance->m_ApperPos = Pos;
+		pInstance->m_Switch = false;
 		pInstance->SetFilePath("data\\MODEL\\test.x");
+
+		// 初期化処理
 		pInstance->Init();
+
+		// インスタンスを返す
 		return pInstance;
 	}
 	else
--- uplift.h
+++ uplift.h
@@ -1,24 +1,40 @@
+//****************************************************************
+//
+// リフトの処理[uplift.h]
+// Author Kensaku Hatori
+//
+//****************************************************************
+
+// 二重インクルード防止
 #ifndef _UPLIFT_H_
 #define _UPLIFT_H_
 
+// インクルード
 #include "objectX.h"
 
+// 上昇リフト
 class CUpLift : public CObjectX
 {
 public:
+    // コンストラクタ・デストラクタ
     CUpLift();
     ~CUpLift();
 
+    // メンバ関数
     HRESULT Init(void);
     void Uninit(void);
     void Update(void);
     void Draw(void);
 
+    // 静的メンバ関数
     static CUpLift* Create(D3DXVECTOR3 Pos,D3DXVECTOR3 Velocity,float Limit);
 private:
-    float m_Limit;
-    D3DXVECTOR3 m_Velocity;
-    std::unique_ptr<btCollisionShape> m_CollisionShape;
-    std::unique_ptr<btRigidBody> m_RigitBody;		// リジットボディー
+    // メンバ変数
+    float m_Limit;                                      // どのくらいで往復するか
+    bool m_Switch;                                      // 往復用のフラグ
+    D3DXVECTOR3 m_Velocity;                             // 移動方向
+    D3DXVECTOR3 m_ApperPos;                             // 初期位置
+    std::unique_ptr<btCollisionShape> m_CollisionShape; // 当たり判定の形状
+    std::unique_ptr<btRigidBody> m_RigitBody;		    // リジットボディー
 };
 #endif // !_UPLIFT_H_
