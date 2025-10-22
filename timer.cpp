4f08e7008f3180ee663a3ccee75e79fd6027ef09
IrisawaKazan <241301@st.yoshida-g.ac.jp>
2025-10-22 10:59:46 +0900

    f

--- timer.cpp
+++ timer.cpp
@@ -1,163 +1,293 @@
-////==============================================================
-////
-//// [timer.cpp]
-//// Author: Irisawa Kazan
-////
-////==============================================================
-//#include"timer.h"
-//#include"number.h"
-//#include"renderer.h"
-//#include"manager.h"
+//==============================================================
 //
-//// 静的メンバ変数宣言
-//LPDIRECT3DTEXTURE9 CTimer::m_pTexture = nullptr;
-//CNumber* CTimer::m_apNumber[MAX_TIMER] = {};
+// [timer.cpp]
+// Author: Irisawa Kazan
 //
-////----------------------------------------
-//// コンストラクタ
-////----------------------------------------
-//CTimer::CTimer(int nPriority) : CObject(nPriority)
-//{
-//	m_pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
-//	m_fxSize = 0.0f;
-//	m_fySize = 0.0f;
-//	m_nTimer = NULL;
-//	m_nContTimer = NULL;
-//}
-//
-////----------------------------------------
-//// デストラクタ
-////----------------------------------------
-//CTimer::~CTimer()
-//{
-//
-//}
-//
-////----------------------------------------
-//// タイマーの生成処理
-////----------------------------------------
-//CTimer* CTimer::Create(D3DXVECTOR3 pos, float xsize, float ysize)
-//{
-//	CTimer* pTimer = nullptr;
-//
-//	if (pTimer == nullptr)
-//	{
-//		pTimer = new CTimer;
-//	}
-//
-//	pTimer->m_pos = pos;
-//	pTimer->m_fxSize = xsize;
-//	pTimer->m_fySize = ysize;
-//
-//	pTimer->Init();
-//
-//	return pTimer;
-//}
-//
-////----------------------------------------
-//// タイマーの初期化処理
-////----------------------------------------
-//HRESULT CTimer::Init(void)
-//{
-//	m_nContTimer = 0; // 初期タイム
-//
-//	float fTexPos = m_fySize / MAX_TIMER;
-//
-//	for (int nCnt = 0; nCnt < MAX_TIMER; nCnt++)
-//	{
-//		m_apNumber[nCnt] = new CNumber;
-//		m_apNumber[nCnt]->SetSize(fTexPos, m_fxSize);
-//		m_apNumber[nCnt]->SetPosition(m_pos);
-//		m_apNumber[nCnt]->Init(D3DXVECTOR3(m_pos.x + (fTexPos * 2.0f * nCnt), m_pos.y, 0.0f), fTexPos, m_fxSize);
-//
-//		m_apNumber[nCnt]->BindTexture(m_pTexture);
-//	}
-//
-//	// タイプの設定
-//	SetType(TYPE_TIMER);
-//
-//	return S_OK;
-//}
-//
-////----------------------------------------
-//// タイマーの終了処理
-////----------------------------------------
-//void CTimer::Uninit(void)
-//{
-//	for (int nCnt = 0; nCnt < MAX_TIMER; nCnt++)
-//	{
-//		if (m_apNumber[nCnt] != nullptr)
-//		{
-//			m_apNumber[nCnt]->Uninit();
-//
-//			delete m_apNumber[nCnt];
-//			m_apNumber[nCnt] = nullptr;
-//		}
-//	}
-//
-//	CObject::Release();
-//}
-//
-////----------------------------------------
-//// タイマーの更新処理
-////----------------------------------------
-//void CTimer::Update(void)
-//{
-//	AddTimer();
-//}
-//
-////----------------------------------------
-//// タイマーの描画処理
-////----------------------------------------
-//void CTimer::Draw(void)
-//{
-//	for (int nCnt = 0; nCnt < MAX_TIMER; nCnt++)
-//	{
-//		if (m_apNumber[nCnt] != nullptr)
-//		{
-//			m_apNumber[nCnt]->Draw();
-//		}
-//	}
-//}
-//
-////----------------------------------------
-//// タイマーの位置の設定処理
-////----------------------------------------
-//void CTimer::SetPosition(D3DXVECTOR3 pos)
-//{
-//	m_pos = pos;
-//}
-//
-////----------------------------------------
-//// タイマーの位置の取得処理
-////----------------------------------------
-//D3DXVECTOR3 CTimer::GetPos(void)
-//{
-//	return m_pos;
-//}
-//
-////----------------------------------------
-//// タイマーの加算処理
-////----------------------------------------
-//void CTimer::AddTimer(void)
-//{
-//	int texU[MAX_TIMER] = { NULL };
-//
-//	m_nTimer++;
-//
-//	if (m_nTimer >= 60)
-//	{
-//		//m_nContTimer--; // カウントダウン
-//		m_nContTimer++; // カウントアップ
-//		m_nTimer = 0;
-//	}
-//
-//	texU[0] = m_nContTimer % 1000 / 100;
-//	texU[1] = m_nContTimer % 100 / 10;
-//	texU[2] = m_nContTimer % 10 / 1;
-//
-//	for (int nCnt = 0; nCnt < MAX_TIMER; nCnt++)
-//	{
-//		m_apNumber[nCnt]->Timer(texU[nCnt]);
-//	}
-//}
+//==============================================================
+#include"timer.h"
+#include"number.h"
+#include"manager.h"
+
+// 静的メンバ変数宣言
+CNumber* CTimer::m_pNumber1[MAX_TIMER] = {};
+CNumber* CTimer::m_pNumber2[MAX_TIMER] = {};
+CNumber* CTimer::m_pNumber3 = {};
+int CTimer::m_nTimer = NULL;
+
+//----------------------------------------
+// コンストラクタ
+//----------------------------------------
+CTimer::CTimer(int nPriority) : CObject(nPriority)
+{
+	m_pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+	m_nTime = NULL;
+	m_nNs = NULL;
+	m_nMin = NULL;
+}
+
+//----------------------------------------
+// デストラクタ
+//----------------------------------------
+CTimer::~CTimer()
+{
+
+}
+
+//----------------------------------------
+// タイマーの生成処理
+//----------------------------------------
+CTimer* CTimer::Create(D3DXVECTOR3 pos)
+{
+	CTimer* pTimer = nullptr;
+
+	if (pTimer == nullptr)
+	{
+		pTimer = new CTimer;
+	}
+
+	pTimer->m_pos = pos;
+
+	pTimer->Init();
+
+	return pTimer;
+}
+
+//----------------------------------------
+// タイマーの初期化処理
+//----------------------------------------
+HRESULT CTimer::Init(void)
+{
+	m_pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+
+	m_nMin = 0;
+
+	m_nTimer = 0;
+
+	for (int nCnt = 0; nCnt < MAX_TIMER; nCnt++)
+	{
+		m_pNumber1[nCnt] = new CNumber;
+
+		if (m_pNumber1[nCnt] != nullptr)
+		{
+			m_pNumber1[nCnt]->Init(200.0f, 200.0f, nCnt, 50.0f, 50.0f, MAX_TIMER, 4, "data\\TEXTURE\\number005.png", 0.1f);
+		}
+
+		m_pNumber2[nCnt] = new CNumber;
+
+		if (m_pNumber2[nCnt] != nullptr)
+		{
+			m_pNumber2[nCnt]->Init(50.0f, 50.0f, nCnt, 50.0f, 50.0f, MAX_TIMER, 4, "data\\TEXTURE\\number005.png", 0.1f);
+		}
+	}
+
+	m_pNumber3 = new CNumber;
+
+	if (m_pNumber3 != nullptr)
+	{
+		m_pNumber3->Init(150.0f, 200.0f, 0, 1.0f, 0.0f, 1, 0, "data\\TEXTURE\\koron.png", 1.0f);
+	}
+
+	return S_OK;
+}
+
+//----------------------------------------
+// タイマーの終了処理
+//----------------------------------------
+void CTimer::Uninit(void)
+{
+	for (int nCnt = 0; nCnt < MAX_TIMER; nCnt++)
+	{
+		if (m_pNumber1[nCnt] != nullptr)
+		{
+			m_pNumber1[nCnt]->Uninit();
+
+			delete m_pNumber1[nCnt];
+			m_pNumber1[nCnt] = nullptr;
+		}
+
+		if (m_pNumber2[nCnt] != nullptr)
+		{
+			m_pNumber2[nCnt]->Uninit();
+
+			delete m_pNumber2[nCnt];
+			m_pNumber2[nCnt] = nullptr;
+		}
+	}
+
+	if (m_pNumber3 != nullptr)
+	{
+		m_pNumber3->Uninit();
+
+		delete m_pNumber3;
+		m_pNumber3 = nullptr;
+	}
+
+	CObject::Release();
+}
+
+//----------------------------------------
+// タイマーの更新処理
+//----------------------------------------
+void CTimer::Update(void)
+{
+	m_nNs++;
+
+	// 1秒経過
+	if (m_nNs > 60)
+	{
+		SubNs(1);
+
+		m_nNs = 0;
+	}
+
+	// 1分経過
+	if (m_nTime >= 60)
+	{
+		SubNs(-60);
+		SubMin(1);
+
+		m_nTime = 0;
+	}
+}
+
+//----------------------------------------
+// タイマーの描画処理
+//----------------------------------------
+void CTimer::Draw(void)
+{
+	for (int nCnt = 0; nCnt < MAX_TIMER; nCnt++)
+	{
+		m_pNumber1[nCnt]->Draw();
+		m_pNumber2[nCnt]->Draw();
+	}
+
+	m_pNumber3->Draw();
+}
+
+//----------------------------------------
+// タイマーの位置の設定処理
+//----------------------------------------
+void CTimer::SetPosition(D3DXVECTOR3 pos)
+{
+	m_pos = pos;
+}
+
+//----------------------------------------
+// タイマーの位置の取得処理
+//----------------------------------------
+D3DXVECTOR3 CTimer::GetPos(void)
+{
+	return m_pos;
+}
+
+//----------------------------------------
+// タイマーの設定処理
+//----------------------------------------
+void CTimer::SetTime(int nTime)
+{
+	// 秒数
+	int aPosTexU[MAX_TIMER] = {};	// 各桁の数字を格納
+	int nData = 100;				// 3桁
+	int nData1 = 10;				// 2桁
+
+	m_nTime -= nTime;
+
+	for (int nCnt1 = 0; nCnt1 < MAX_TIMER; nCnt1++)
+	{
+		if (nCnt1 == 0)
+		{
+			// 0番目だったら
+			aPosTexU[0] = m_nTime / nData;
+		}
+		else
+		{
+			// 0番目以外
+			aPosTexU[nCnt1] = (m_nTime % nData) / nData1;
+			nData = nData / 10;
+			nData1 = nData1 / 10;
+		}
+
+		m_pNumber2[nCnt1]->SetNumber(aPosTexU[nCnt1], 4);
+	}
+
+	int nData2 = 100;
+	int nData3 = 10;
+	
+	for (int nCnt2 = 0; nCnt2 < MAX_TIMER; nCnt2++)
+	{
+		if (nCnt2 == 0)
+		{
+			// 0番目だったら
+			aPosTexU[0] = m_nMin / nData2;
+		}
+		else
+		{
+			// 0番目以外
+			aPosTexU[nCnt2] = (m_nMin % nData2) / nData3;
+			nData2 = nData2 / 10;
+			nData3 = nData3 / 10;
+		}
+
+		m_pNumber1[nCnt2]->SetNumber(aPosTexU[nCnt2], 4);
+	}
+}
+
+//----------------------------------------
+// タイマーの秒減算処理
+//----------------------------------------
+void CTimer::SubNs(int nValue)
+{
+	int aPosTexU[MAX_TIMER] = {};
+	int nData = 100;
+	int nData1 = 10;
+
+	m_nTime = nValue;
+
+	for (int nCnt = 0; nCnt < MAX_TIMER; nCnt++)
+	{
+		// 0番目以外
+		aPosTexU[nCnt] = (m_nTime % nData) / nData1;
+		nData = nData / 10;
+		nData1 = nData1 / 10;
+
+		m_pNumber1[nCnt]->SetNumber(aPosTexU[nCnt], 4);
+	}
+}
+
+//----------------------------------------
+// タイマーの分減算処理
+//----------------------------------------
+void CTimer::SubMin(int nValue)
+{
+	int aPosTexU[MAX_TIMER] = {};
+	int nData = 100;
+	int nData1 = 10;
+
+	m_nMin = nValue;
+
+	for (int nCnt = 0; nCnt < MAX_TIMER; nCnt++)
+	{
+		// 0番目以外
+		aPosTexU[nCnt] = (m_nMin % nData) / nData1;
+		nData = nData / 10;
+		nData1 = nData1 / 10;
+
+		m_pNumber2[nCnt]->SetNumber(aPosTexU[nCnt], 4);
+	}
+}
+
+//----------------------------------------
+// タイマーの秒数取得処理
+//----------------------------------------
+int CTimer::GetNs(void)
+{
+	return m_nNs;
+}
+
+//----------------------------------------
+// タイマーの分数取得処理
+//----------------------------------------
+int CTimer::GetMin(void)
+{
+	return m_nMin;
+}
--- timer.h
+++ timer.h
@@ -1,47 +1,55 @@
-////==============================================================
-////
-//// [timer.h]
-//// Author: Irisawa Kazan
-////
-////==============================================================
-//#ifndef _TIMER_H_ // このマクロ定義がされてなかったら
-//#define _TIMER_H_ // 2重インクルード防止のマクロ定義
+//==============================================================
 //
-//#include"main.h"
-//#include"object.h"
+// [timer.h]
+// Author: Irisawa Kazan
 //
-//// 前方宣言
-//class CNumber;
-//
-//// マクロ定義
-//#define MAX_TIMER (3)
-//
-//// タイマークラス
-//class CTimer : public CObject
-//{
-//public:
-//	CTimer(int nPriority = 7);
-//	~CTimer();
-//
-//	static CTimer* Create(D3DXVECTOR3 pos, float xsize, float ysize);
-//
-//	HRESULT Init(void);
-//	void Uninit(void);
-//	void Update(void);
-//	void Draw(void);
-//
-//	void SetPosition(D3DXVECTOR3 pos);
-//	D3DXVECTOR3 GetPos(void);
-//
-//	void AddTimer(void);
-//
-//private:
-//	D3DXVECTOR3 m_pos;
-//	float m_fxSize;
-//	float m_fySize;
-//	static CNumber* m_apNumber[MAX_TIMER];
-//	int m_nTimer;
-//	int m_nContTimer;
-//};
-//
-//#endif
+//==============================================================
+#ifndef _TIMER_H_ // このマクロ定義がされてなかったら
+#define _TIMER_H_ // 2重インクルード防止のマクロ定義
+
+#include"main.h"
+#include"object.h"
+
+// 前方宣言
+class CNumber;
+
+// マクロ定義
+#define MAX_TIMER (2)
+
+// タイマークラス
+class CTimer : public CObject
+{
+public:
+	CTimer(int nPriority = 7);
+	~CTimer();
+
+	static CTimer* Create(D3DXVECTOR3 pos);
+
+	HRESULT Init(void);
+	void Uninit(void);
+	void Update(void);
+	void Draw(void);
+
+	void SetPosition(D3DXVECTOR3 pos);
+	D3DXVECTOR3 GetPos(void);
+
+	void SetTime(int nTime);
+	void SubNs(int nValue);
+	void SubMin(int nValue);
+
+	int GetNs(void);
+	int GetMin(void);
+	static int GetTime(void) { return m_nTimer; }
+
+private:
+	static CNumber* m_pNumber1[MAX_TIMER];	// ナンバーのポインタ(秒)
+	static CNumber* m_pNumber2[MAX_TIMER];	// ナンバーのポインタ(分)
+	static CNumber* m_pNumber3;				// ナンバーのポインタ(:)
+	D3DXVECTOR3 m_pos;						// 位置
+	int m_nTime;							// 値
+	int m_nNs;								// 秒
+	int m_nMin;								// 分
+	static int m_nTimer;					// タイマーの取得
+};
+
+#endif
