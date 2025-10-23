a41ab77940985d08dd5457a06fe81e4f55c54fb6
KensakuHatori <241316@st.yoshida-g.ac.jp>
2025-10-23 14:22:51 +0900

    ギミック

--- gimmick.h
+++ gimmick.h
@@ -1,16 +1,14 @@
 //=================================================
 //
 //	block.h
-// 
+//
 // Author:chikada shouya
 //
 //=================================================
 #ifndef _GIMMICK_H_
 #define _GIMMICK_H_
-
 #include "main.h"
 #include "block.h"
-
 //マクロ
 #define HALF (0.5f)					// 半分の値
 
@@ -29,28 +27,17 @@ public:
 		TYPE_4,			//
 		TYPE_MAX,
 	}TYPE;
-
 	CGimmick(int nPriority = 7);
 	~CGimmick();
 	HRESULT Init(void);
-	void InitRB(void);
 	void Uninit(void);
 	void Update(void);
 	void Draw(void);
-
 	static CGimmick* Create(std::string sName, D3DXVECTOR3 pos, D3DXVECTOR3 rot, D3DXVECTOR3 Scale = { 1.0f,1.0f,1.0f });
-
 private:
 	D3DXVECTOR3 m_pos;		// 位置
 	D3DXVECTOR3 m_rot;		// 向き
 	TYPE m_type;			// ブロックのタイプ
-	D3DXVECTOR3 m_size;		// サイズ
-	D3DXVECTOR3 m_VtxMax;	// 頂点最大値
-	D3DXVECTOR3 m_VtxMin;	// 頂点最小値
-	D3DXVECTOR3 m_RBOffset;	// オフセット
 	std::string sNamePath;	// ファイルパス
-	int m_nIdx;				// インデックス
-	std::unique_ptr<btCollisionShape> m_CollisionShape;	// 当たり判定の形状
-	std::unique_ptr<btRigidBody> m_RigitBody;			// リジットボディー
 };
 #endif
