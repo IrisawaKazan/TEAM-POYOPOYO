caf60781995b0b2390be20f82927707b107fdcca
IrisawaKazan <241301@st.yoshida-g.ac.jp>
2025-11-19 16:51:52 +0900

    sound追加

--- sound.cpp
+++ sound.cpp
@@ -23,6 +23,7 @@ CSound::SOUNDINFO CSound::m_aSoundInfo[CSound::LABEL_MAX] =
 	{"data/SE/pauseSe.wav", 0},				// ポーズSE
 	{"data/SE/fallingSe.wav", 0},			// 落下SE
 	{"data/SE/switchSe.wav", 0},			// スイッチSE
+	{"data/SE/turn_over.wav", 0},			// チュートリアルのページめくりSE
 };	
 
 //*********************************************
