c0848167bb2decc66bc0704d0fe266329cf166d6
近田尚也 <241313@st.yoshida-g.ac.jp>
2025-11-21 09:59:48 +0900

    a

--- number.h
+++ number.h
@@ -44,6 +44,7 @@ public:
 private:
 	LPDIRECT3DVERTEXBUFFER9 m_pVtxBuff;		// 頂点バッファ
 	D3DXVECTOR3 m_pos[2];					// 位置
+	D3DXVECTOR3 m_Apper[2];					// 初期位置
 	D3DXVECTOR3 m_Dest;						// 目的値
 	TYPE m_Type;							// タイプ
 	bool m_bChange;							// カラー判定用
