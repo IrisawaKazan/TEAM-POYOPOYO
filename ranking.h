5f5f5dafe017bba7657c48e35015eda80f2d0f47
fs820 <241308@st.yoshida-g.ac.jp>
2025-11-27 14:36:28 +0900

    bin

--- ranking.h
+++ ranking.h
@@ -18,7 +18,7 @@ class CObject2D; //
 class CRanking : CObject
 {
 public:
-	CRanking(size_t timeCount, int priority) : CObject(priority), m_timeCount{ timeCount }, m_pBrackboard{}, m_pNow{}, m_pRankings{}, m_pRankNumbers{}, m_pos{}, m_rankEndPos{}, m_isRankNoveEnds{} {}
+	CRanking(size_t timeCount, int priority) : CObject(priority), m_timeCount{ timeCount }, m_isColorFlag{}, m_counter{}, m_colCounter{}, m_pBrackboard{}, m_pNow{}, m_pRankings{}, m_pRankNumbers{}, m_pos{}, m_rankEndPos{}, m_isRankNoveEnds{} {}
 	~CRanking() = default;
 
 	static CRanking* Create(D3DXVECTOR3 pos, D3DXVECTOR2 size, size_t timeCount, int priority = 7);
@@ -52,9 +52,11 @@ private:
 	static constexpr const char* TEXTURE_PATH = "data\\TEXTURE\\number001.png"; // テクスチャ
 	static constexpr float NUMBER_SCALE = 0.0008f;                              // 大きさ
 	static constexpr float NUMBER_MOVE_START_OFFSET = 0.5f;                     // 動き始めのオフセット
+	static constexpr size_t NUMBER_MOVE_C = 120;                                // カラーカウント
+	static constexpr size_t NUMBER_COLOR_C = 30;                                // カラーカウント
+	static const D3DXCOLOR NUMBER_COLOR;                                        // カラー
 	static const D3DXVECTOR2 TEXTURE_SIZE;                                      // テクスチャサイズ
 	static const D3DXVECTOR2 TEXTURE_UV_COUNT;                                  // テクスチャ分割
-
 
 	// 使う数字について (ランクナンバー)
 	static constexpr const char* RN_TEXTURE_PATH = "data\\TEXTURE\\RankNum.png";  // テクスチャ
@@ -81,6 +83,9 @@ private:
 	std::array<D3DXVECTOR3, MAX_RANKING + 1u> m_rankEndPos;   // 最終位置
 	std::array<bool, MAX_RANKING + 1u> m_isRankNoveEnds;      // 動き終わったか?
 	std::array<CNumber*, MAX_RANKING> m_pRankNumbers;         // ランキングの1,2,3,4,5配列
+	size_t m_counter;                                         // カウンター
+	size_t m_colCounter;                                      // カウンター     
+	bool m_isColorFlag;                                       // 空フラグ
 	D3DXVECTOR3 m_pos;				                          // 位置
 	D3DXVECTOR2 m_size;				                          // 大きさ
 };
