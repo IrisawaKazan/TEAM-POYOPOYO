3dd0e202f1ccda3e353f14db18485944a98867ca
KensakuHatori <241316@st.yoshida-g.ac.jp>
2025-11-27 14:33:56 +0900

    game

--- game.cpp
+++ game.cpp
@@ -97,32 +97,6 @@ HRESULT CGame::Init(void)
 	CManager::GetRenderer()->GetBackBufferSize(&screenSize);
 	pTimer = CTimer::Create(D3DXVECTOR3(screenSize.x * 0.1f, screenSize.y * 0.05f, 0.0f), 2, 0, TIME_LIMIT);
 
-	//// 火の情報を設定
-	//CParticle3D::DefoultEffectInfo FireInfo;
-	//FireInfo.Bece.Col = FIRE;
-	//FireInfo.Bece.fMaxSpeed = 1.0f;
-	//FireInfo.Bece.fMinSpeed = 1.0f;
-	//FireInfo.Bece.MaxDir = { 0.1f,0.5f,0.1f };
-	//FireInfo.Bece.MinDir = { -0.1f,0.5f,-0.1f };
-	//FireInfo.Bece.nLife = 1;
-	//FireInfo.Bece.nMaxLife = 30;
-	//FireInfo.Bece.nMinLife = 30;
-	//FireInfo.Bece.nNumEffect = 5;
-	//FireInfo.Bece.Pos = VEC3_NULL;
-	//FireInfo.MaxRadius = 15.0f;
-	//FireInfo.MinRadius = 15.0f;
-	//FireInfo.Bece.bLoop = true;
-	//FireInfo.Bece.nCoolDown = 2;
-	//FireInfo.Bece.Gravity = 0.0f;
-	//memcpy(FireInfo.Bece.FilePath, CEffect3D::Config::Smoke, sizeof(FireInfo.Bece.FilePath));
-	//FireInfo.Bece.nPriority = 1;
-
-	//// 生成
-	//CParticle3D::Create(FireInfo);
-
-	//// アイテムの生成処理 Misaki
-	//CItem::Create(CItem::ITEM_LEFT, D3DXVECTOR3(0.0f, 50.0f, 100.0f), D3DXVECTOR3(0.0f, 0.0f, 0.0f), D3DXVECTOR3(1.2f, 1.2f, 1.2f), "data\\Model\\item\\item000.x");
-
 #ifdef _DEBUG
 #else
 #endif // _DEBUG
@@ -179,10 +153,6 @@ void CGame::Update(void)
 				CFade::SetFade(new CResult);
 			}
 		}
-		//if (CManager::GetInputKeyboard()->GetTrigger(DIK_F3) == true && m_pTutorialBoard->GetProgress() == false)
-		//{
-		//	m_pTutorialBoard->SetUp("data\\TEXTURE\\tutorial_001.png", true);
-		//}
 		if (CManager::GetInputKeyboard()->GetTrigger(DIK_SPACE) ||
 			CManager::GetInputJoypad()->GetTrigger(CInputJoypad::JOYKEY_X) ||
 			CManager::GetInputJoypad()->GetTrigger(CInputJoypad::JOYKEY_Y) == true && m_pTutorialBoard->GetProgress() == false)
@@ -211,13 +181,6 @@ void CGame::Update(void)
 	{
 		m_pTutorialBoard->Update();
 	}
-
-	//if (CManager::GetInputKeyboard()->GetTrigger(DIK_SPACE) == true)
-	//{
-	//	//CManager::GetSound()->Play(CSound::LABEL_ENTER);
-	//	m_bGoal = true;
-	//	CFade::SetFade(new CResult);
-	//}
 
 	if (pTimer->IsTimeOver())
 	{// タイムオーバー
